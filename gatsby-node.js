97d84d677e1d8c5b7da5da80a40675ed8de4c46d
Cherry <cherry@alli.io>
2023-11-01 12:24:18 +0900

    pros

--- gatsby-node.js
+++ gatsby-node.js
@@ -81,7 +81,7 @@ exports.createPages = async ({ graphql, actions }) => {
         modalities: modality.data.alliApi.modalities,
         specializations: specialization.data.alliApi.specializations,
       },
-    });
+    })
 
     data.alliApi.availablePros.forEach((pro) => {
       createPage({
