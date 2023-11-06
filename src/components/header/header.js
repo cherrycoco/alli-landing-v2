0f95957d00854dd18ef65c62f3b583113c194375
Cherry <cherry@alli.io>
2023-11-05 19:52:00 -0500

    logo external link

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -7,9 +7,9 @@ const Header = () => {
   return (
     <header className="bg-white sticky top-0 z-10 drop-shadow-md">
       <nav className="mx-auto flex items-center justify-between p-6 lg:px-8" aria-label="Global">
-        <Link to='/'>
+        <a href='https://alli.io/'>
           <img className='mb-0' src='https://res.cloudinary.com/dhze7gimq/image/upload/v1627495979/alli_landing/alli-logo_gho3wu_1_mm0ius.png' alt='alli logo'/>
-        </Link>
+        </a>
         <div className="items-center flex gap-x-6">
           <Link to='/therapists'>
             <span className="text-sm leading-6 text-gray-900">Our Therapists</span>
